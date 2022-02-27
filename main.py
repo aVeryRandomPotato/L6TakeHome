5e672cea00cd274196cd0a52eb347a12b020b22d
aVeryRandomPotato <59588611+aVeryRandomPotato@users.noreply.github.com>
2022-02-27 15:18:11 -0700

    Added Basic Flask

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+app.secret_key = "hu90bv2yb0824y0BYUG0h9EGWH&igfRG&2h-GRW&5*Hg08yWRhy80GRWhy80RGWhwqaety80"
+
+if __name__ == "__main__":
+    app.run(host="localhost", port="8693")
