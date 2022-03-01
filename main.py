6413684e5bbb6388834e997debbf95747d9b5ca4
aVeryRandomPotato <chase.stancik@gmail.com>
2022-03-01 11:31:30 -0700

    fixed return login

--- main.py
+++ main.py
@@ -62,6 +62,8 @@ def login():
         
         else:
             return render_template("login.html", message = "Invalid username and/or password")
+    else:
+        renturn render_template("login.html")
     
 
 @app.route("/logout")
